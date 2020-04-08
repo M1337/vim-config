a892ee83b30a52635579daecdd940ab9263d5601
Matthias <molberzx@localhost.localdomain>
2020-04-08 10:13:25 +0200

    Updated for tmux

---
+++ setup_tmux.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+cp ./tmuxconf ~/.tmuxconf
+
+echo "In tmux press Prefix-I to install plugins"
+
--- setup_vim.sh
+++ setup_vim.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp ./vimrc ~/.vim/vimrc
-ln ~/.vimrc ~/.vim/vimrc
+cp ./vimrc ~/.vimrc
 vim +PluginInstall +qall
 
 echo "dont forget to install / update YoucompleteMe"
